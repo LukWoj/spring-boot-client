27e3ca842789d10fa175bbad5d7786d4b5cc95a7
Łukasz <lukasz.woje@wp.pl>
2018-03-07 20:48:45 +0100

    rev 1

---
+++ src/main/java/Response.java
@@ -0,0 +1,19 @@
+public class Response {
+
+    private String response;
+
+    public Response() {
+    }
+
+    public Response(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
---
+++ src/main/java/Simulation.java
@@ -0,0 +1,17 @@
+import org.apache.catalina.connector.ResponseFacade;
+import org.springframework.web.client.RestTemplate;
+
+public class Simulation {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8080/";
+        String url1 = url + "test1";
+        Response response = restTemplate.postForObject(url1,null, Response.class);
+        System.out.println(response.getResponse());
+
+
+        String url2 = url + "testParam?name=Donald&surname=Duck";
+        String response2 = restTemplate.postForObject(url2,null, String.class);
+        System.out.println(response2);
+    }
+}
