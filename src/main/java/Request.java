dced78b8dd7a0206ad716a663fdd86eb73d79ddc
Łukasz <lukasz.woje@wp.pl>
2018-03-07 20:56:46 +0100

    rev 2

---
+++ src/main/java/Request.java
@@ -0,0 +1,17 @@
+public class Request {
+    private final String name;
+    private final String surname;
+
+    public Request(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,17 +1,27 @@
-import org.apache.catalina.connector.ResponseFacade;
 import org.springframework.web.client.RestTemplate;
 
 public class Simulation {
+
     public static void main(String[] args) {
+
+
+
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://localhost:8080/";
-        String url1 = url + "test1";
-        Response response = restTemplate.postForObject(url1,null, Response.class);
-        System.out.println(response.getResponse());
-
-
-        String url2 = url + "testParam?name=Donald&surname=Duck";
-        String response2 = restTemplate.postForObject(url2,null, String.class);
+        String paramrRequest = url + "testParam";
+        Request request = new Request("Tomek", "Atomek");
+        String response2 = restTemplate.postForObject(paramrRequest, request, String.class);
         System.out.println(response2);
+
+        // RestTemplate restTemplate = new RestTemplate();
+//        String url = "http://localhost:8080/";
+//        String url1 = url + "test1";
+//        Response response = restTemplate.postForObject(url1,null, Response.class);
+//        System.out.println(response.getResponse());
+//
+//
+//        String url2 = url + "testParam?name=Donald&surname=Duck";
+//        String response2 = restTemplate.postForObject(url2,null, String.class);
+//        System.out.println(response2);
     }
 }
